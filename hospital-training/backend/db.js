c35f43c102392cd78b00bb2be1b61ec20ae1177d
BluseSara <sara.alluhaibi@outlook.com>
2025-04-10 20:04:18 +0300

    .

--- hospital-training/backend/db.js
+++ hospital-training/backend/db.js
@@ -3,8 +3,8 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: "MediServee",  // ✅ لازم يكون مطابق 100% للي في MySQL
+  password: process.env.DB_PASSWORD || "12345678",
+  database: "MediServee", 
   port: process.env.DB_PORT || 3306,
 });
 
