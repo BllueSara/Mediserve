d3848b08289ffc97e31df0127c95cc5d4679b235
BluseSara <sara.alluhaibi@outlook.com>
2025-04-12 21:20:39 +0300

    .

--- hospital-training/backend/server.js
+++ hospital-training/backend/server.js
@@ -856,7 +856,6 @@ app.post("/add-device-model", (req, res) => {
 });
 
 
-
 app.get('/regular-maintenance-summary', (req, res) => {
   const sql = `
     SELECT 
@@ -893,6 +892,30 @@ app.get('/regular-maintenance-summary', (req, res) => {
     res.json(result);
   });
 });
+
+
+app.get('/maintenance-stats', (req, res) => {
+  const sql = `
+    SELECT
+      COUNT(*) AS total,
+      SUM(CASE
+        WHEN CURDATE() > DATE_ADD(last_maintenance_date, INTERVAL 3 MONTH)
+        THEN 1
+        ELSE 0
+      END) AS completed
+    FROM Regular_Maintenance
+    WHERE frequency = '3months';
+  `;
+
+  db.query(sql, (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Error fetching stats' });
+    }
+    res.json(result[0]);
+  });
+});
+
 
 
 
