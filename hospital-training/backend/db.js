5a13f49b267c08364d0165a0f285b923929c4620
SARA ALLUHAIBI <sara.alluhaibi@outlook.com>
2025-04-13 15:52:08 +0300

    return version

--- hospital-training/backend/db.js
+++ hospital-training/backend/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "12345678",
+  password: process.env.DB_PASSWORD || "Rawad12-",
   database: "MediServee", 
   port: process.env.DB_PORT || 3306,
 });
