0de62df872b93c9b3a887a99295d9f737975efeb
SARA ALLUHAIBI <sara.alluhaibi@outlook.com>
2025-05-25 03:18:33 +0300

    all folders are accessible

--- hospital-training/backend/network.js
+++ hospital-training/backend/network.js
@@ -33,8 +33,11 @@ module.exports = authenticateToken;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
-app.use(express.static(__dirname));
+
+// Serve static files from all directories
+app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, '..')));
+app.use(express.static(path.join(__dirname, '..', '..')));
 
 
 
